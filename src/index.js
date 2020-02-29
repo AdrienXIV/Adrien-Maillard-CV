c1a1e42fe42666a4dadcb35557081296807e1f84
adrien@webky.fr <adrien@webky.fr>
2020-02-29 19:35:37 +0100

    deploy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import App from './App'
 import './css/style.css';
 
 const Stage = ()=> <Redirect to={process.env.PUBLIC_URL + '/stage'}></Redirect>;
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <Switch>
             <Route exact path={process.env.PUBLIC_URL + '/'} component={Stage} />
             <Route exact path={process.env.PUBLIC_URL + '/:choice'} component={App} />
         </Switch>
-    </BrowserRouter>, document.getElementById('root'));
+    </HashRouter>, document.getElementById('root'));
