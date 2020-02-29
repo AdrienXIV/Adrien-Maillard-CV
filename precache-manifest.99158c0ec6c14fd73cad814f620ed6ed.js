4aa7a4b0e2308331cb86fdb3916e56dc6f806312
adrien@webky.fr <adrien@webky.fr>
2020-02-29 19:17:12 +0100

    Updates

---
+++ precache-manifest.99158c0ec6c14fd73cad814f620ed6ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d4632e39d720fd205f27c998d5e4c0",
+    "url": "/Adrien-Maillard-CV/index.html"
+  },
+  {
+    "revision": "ce22ed09c603c6fc8cf5",
+    "url": "/Adrien-Maillard-CV/static/css/main.89b491c6.chunk.css"
+  },
+  {
+    "revision": "eeabe61a1b5fdf9ca2ea",
+    "url": "/Adrien-Maillard-CV/static/js/2.3e91915a.chunk.js"
+  },
+  {
+    "revision": "78f8d92e31a21ccb62293ca00cfd53a3",
+    "url": "/Adrien-Maillard-CV/static/js/2.3e91915a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce22ed09c603c6fc8cf5",
+    "url": "/Adrien-Maillard-CV/static/js/main.efbe1ef2.chunk.js"
+  },
+  {
+    "revision": "a813698676303a3c56fc",
+    "url": "/Adrien-Maillard-CV/static/js/runtime-main.9edf374a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Adrien-Maillard-CV/stage/precache-manifest.5e1dc5ef0beb851a845b6e092d7d565a.js"
+  "/Adrien-Maillard-CV/precache-manifest.99158c0ec6c14fd73cad814f620ed6ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Adrien-Maillard-CV/stage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Adrien-Maillard-CV/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
